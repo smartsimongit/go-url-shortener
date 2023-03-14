498d08e6a92e598a433179c0668ece82461a7cd2
s.shandalyuk <s.shandalyuk@yandex.ru>
2023-03-14 20:05:02 +0700

    iter14/delete handler return 202

--- internal/storage/storageDB.go
+++ internal/storage/storageDB.go
@@ -165,7 +165,7 @@ func (r *Repository) PutAll(records []URLRecord, ctx context.Context) error {
 
 func (r *Repository) Delete(ids []string, user string, ctx context.Context) error {
 	_, err := r.pool.Exec(ctx,
-		"UPDATE public.link_pairs SET public.link_pairs.is_deleted = true WHERE id in (ids) AND usr = user VALUES($1,$2)",
+		"UPDATE public.link_pairs SET public.link_pairs.is_deleted = true WHERE id in ($1) AND usr = $2",
 		ids, user)
 	if err != nil {
 		fmt.Println("ошибка записи ", err.Error())
